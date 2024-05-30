05fec725e685312b8f9272affe7812b0b5fa8f07
yonatank93 <kurniawanyo@outlook.com>
2024-05-30 07:32:20 -0600

    Move the utils into main
    Previously, the utils script contains solver command, which I think
    should be in the same file as the main UI script.

--- rubik_solver/main.py
+++ rubik_solver/main.py
@@ -1,8 +1,70 @@
 import argparse
 import time
 
+from .Solver import Solver
+from .Solver import Beginner
+from .Solver import CFOP
+from .Solver import Kociemba
+from .NaiveCube import NaiveCube
+from .Cubie import Cube
 from .Reader import UserInput
-from .utils import METHODS, pprint, solve
+from .Printer import TtyPrinter
+
+__author__ = "Victor Cabezas"
+
+METHODS = {
+    "Beginner": Beginner.BeginnerSolver,
+    "CFOP": CFOP.CFOPSolver,
+    "Kociemba": Kociemba.KociembaSolver,
+}
+
+
+def _check_valid_cube(cube):
+    """Checks if cube is one of str, NaiveCube or Cubie.Cube and returns
+    an instance of Cubie.Cube"""
+
+    if isinstance(cube, str):
+        c = NaiveCube()
+        c.set_cube(cube)
+        cube = c
+
+    if isinstance(cube, NaiveCube):
+        c = Cube()
+        c.from_naive_cube(cube)
+        cube = c
+
+    if not isinstance(cube, Cube):
+        raise ValueError("Cube is not one of (str, NaiveCube or Cubie.Cube)")
+
+    return cube
+
+
+def solve(cube, method=Beginner.BeginnerSolver, *args, **kwargs):
+    if isinstance(method, str):
+        if not method in METHODS:
+            raise ValueError(
+                "Invalid method name, must be one of (%s)" % ", ".join(METHODS.keys())
+            )
+        method = METHODS[method]
+
+    if not issubclass(method, Solver):
+        raise ValueError(
+            "Method %s is not a valid Solver subclass" % method.__class__.__name__
+        )
+
+    if isinstance(cube, UserInput):
+        cube = str(cube)
+    cube = _check_valid_cube(cube)
+
+    solver = method(cube)
+
+    return solver.solution(*args, **kwargs)
+
+
+def pprint(cube, color=True):
+    cube = _check_valid_cube(cube)
+    printer = TtyPrinter(cube, color)
+    printer.pprint()
 
 
 def main(argv=None):
